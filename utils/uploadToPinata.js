6f4db286026cfde90d899f6d001fbdc8c96ab56c
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-08-23 04:34:13 +0500

    Corrected return statement of storeImages function
    Corrected return statement of storeImages function.

--- utils/uploadToPinata.js
+++ utils/uploadToPinata.js
@@ -1,40 +1,41 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const pinataSdk = require("@pinata/sdk")
+const pinataSdk=require("@pinata/sdk");
 
-const path = require("path")
+const path=require("path");
 
-const fs = require("fs")
+const fs=require("fs");
 
-const pinata = pinataSdk(process.env.PINATA_API, process.env.PINATA_API_SECRET)
+const pinata=pinataSdk(process.env.PINATA_API, process.env.PINATA_API_SECRET);
 
-async function storeImages(imgPath) {
-    const fullImages = path.resolve(imgPath)
-    const files = fs.readdirSync(fullImages)
-    let responses = []
+async function storeImages(imgPath){
+    const fullImages=path.resolve(imgPath);
+    const files=fs.readdirSync(fullImages);
+    let responses=[];
     console.log("Processing Pinata")
 
-    for (let i = 0; i < files.length; i++) {
-        console.log(`Working on ${files[i]}`)
-        const readableFile = fs.createReadStream(`${fullImages}/${files[i]}`)
-        try {
-            const tx = await pinata.pinFileToIPFS(readableFile)
+    for(let i=0; i<files.length; i++){
+        console.log(`Working on ${files[i]}`);
+        const readableFile= fs.createReadStream(`${fullImages}/${files[i]}`);
+        try{
+            const tx=await pinata.pinFileToIPFS(readableFile);
             responses.push(tx)
-        } catch (e) {
-            console.log(e)
+        }
+        catch(e){
+            console.log(e);
         }
     }
-    return { responses, files }
+    return{responses, files};
 }
 
-async function storeUri(metadata) {
-    try {
-        const response = await pinata.pinJSONToIPFS(metadata)
-        return response
-    } catch (e) {
-        console.log(e)
+async function storeUri(metadata){
+    try{
+        const response=await pinata.pinJSONToIPFS(metadata);
+        return response;
+    }catch(e){
+        console.log(e);
     }
-    return null
+    return null;
 }
 
-module.exports = { storeImages, storeUri }
+module.exports={storeImages, storeUri}
